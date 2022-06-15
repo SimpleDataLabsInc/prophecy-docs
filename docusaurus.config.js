7b3cc191b2b2075df3f93ec0d4dfab8a08d91427
Scott Lyons <scottalyons@gmail.com>
2022-06-15 16:53:10 -0700

    Linting/prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,89 +1,95 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Prophecy',
-  tagline: 'Low Code Data Engineering',
-  url: 'https://beta.docs.prophecy.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
+  title: "Prophecy",
+  tagline: "Low Code Data Engineering",
+  url: "https://beta.docs.prophecy.io/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js')
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
-  stylesheets: ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"],
+  stylesheets: [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+  ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'Prophecy Logo',
-          src: 'img/Prophecy Logo.png',
-          srcDark: 'img/Prophecy Logo White.png',
+          alt: "Prophecy Logo",
+          src: "img/Prophecy Logo.png",
+          srcDark: "img/Prophecy Logo White.png",
         },
         items: [
-          {to: 'https://www.prophecy.io/', position: 'left', label: 'Home'},
-          {type: 'doc', docId: 'index', position: 'left', label: 'Docs'},
-          {to: 'https://www.prophecy.io/blogs', label: 'Blog', position: 'left'},
-          {to: 'http://app.prophecy.io/', label: 'Login', position: 'right'}
+          { to: "https://www.prophecy.io/", position: "left", label: "Home" },
+          { type: "doc", docId: "index", position: "left", label: "Docs" },
+          {
+            to: "https://www.prophecy.io/blogs",
+            label: "Blog",
+            position: "left",
+          },
+          { to: "http://app.prophecy.io/", label: "Login", position: "right" },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Slack',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              }
+                label: "Slack",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Login',
-                href: 'https://app.prophecy.io/',
+                label: "Login",
+                href: "https://app.prophecy.io/",
               },
             ],
           },
@@ -93,7 +99,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['java','scala'],
+        additionalLanguages: ["java", "scala"],
       },
     }),
 };
