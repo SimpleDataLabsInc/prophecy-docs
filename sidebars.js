d5517417b6d1533ecbf7b5d27f272fc8200a75d8
kathweinschenkprophecy <katherine@prophecy.io>
2025-06-04 12:45:22 -0400

    4.1 charts for interims and business apps (#676)
    * v1 charts for apps
    
    * Add page on charts
    
    * charts and app component updates
    
    * add displacement map info
    
    * expanded chart interims (#673)
    
    * expanded charts section
    
    * KW suggestions
    
    ---------
    
    Co-authored-by: Kailie Yuan <kailieyuan@mac.mynetworksettings.com>
    
    * Clarify how to add charts to apps
    
    * Add screenshots to apps
    
    * KY review
    
    * Revise app builder info
    
    * Add info on preview option
    
    ---------
    
    Co-authored-by: Kailie Yuan <kailie@prophecy.io>
    Co-authored-by: Kailie Yuan <kailieyuan@mac.mynetworksettings.com>

--- sidebars.js
+++ sidebars.js
@@ -270,7 +270,26 @@ const sidebars = {
             type: "doc",
             id: "analysts/development/data-explorer/data-explorer",
           },
-          items: ["analysts/development/data-explorer/data-profile"],
+          items: [
+            "analysts/development/data-explorer/data-profile",
+            {
+              type: "category",
+              label: "Charts",
+              link: {
+                type: "doc",
+                id: "analysts/development/data-explorer/charts/charts",
+              },
+              items: [
+                "analysts/development/data-explorer/charts/area-chart",
+                "analysts/development/data-explorer/charts/bar-chart",
+                "analysts/development/data-explorer/charts/candlestick-chart",
+                "analysts/development/data-explorer/charts/line-chart",
+                "analysts/development/data-explorer/charts/map-chart",
+                "analysts/development/data-explorer/charts/pie-chart",
+                "analysts/development/data-explorer/charts/scatter-chart",
+              ],
+            },
+          ],
         },
         {
           type: "category",
@@ -327,15 +346,18 @@ const sidebars = {
     },
     {
       type: "category",
-      label: "Apps",
+      label: "Prophecy Apps",
       link: { type: "doc", id: "analysts/business-apps/business-apps" },
       items: [
         "analysts/business-apps/run-apps",
         {
           type: "category",
-          label: "Create apps",
-          link: { type: "doc", id: "analysts/business-apps/app-creation" },
-          items: ["analysts/business-apps/app-components"],
+          label: "App Builder",
+          link: { type: "doc", id: "analysts/business-apps/app-builder" },
+          items: [
+            "analysts/business-apps/app-creation",
+            "analysts/business-apps/app-components",
+          ],
         },
       ],
     },
