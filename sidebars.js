1bfbbc630ffae29388b7879efba9ccf286ba696e
kathweinschenkprophecy <katherine@prophecy.io>
2025-09-11 16:51:55 -0400

    AI agent additional content (#816)
    * agent quick start v1
    
    * Commit gifs
    
    * Add information about chat UI
    
    * DB review

--- sidebars.js
+++ sidebars.js
@@ -43,6 +43,7 @@ const sidebars = {
             id: "getting-started/quick-starts/quick-starts",
           },
           items: [
+            "getting-started/quick-starts/agent-quick-start",
             "getting-started/quick-starts/analysts-qs",
             "getting-started/quick-starts/engineers-qs",
             "getting-started/quick-starts/databricks-partner-connect",
